af887c4928b00084807876f17df289f643dc0318
陈晓宇 <cxy@chenxiaoyudeMacBook-Pro.local>
2021-05-26 15:25:34 +0800

    first connect

---
+++ 起名大全/起名大全/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  起名大全
+//
+//  Created by 陈晓宇 on 2021/5/26.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
